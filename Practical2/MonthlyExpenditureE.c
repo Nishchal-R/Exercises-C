cdfb367344ca389a35711666cd69e0860ca50f1a
Nishchal Ranjitkar <Nishchal-R@users.noreply.github.com>
2026-02-21 09:35:27 +0545

    Updated Practical2 (e) to Part-3 (3) as per instruction

--- Practical2/MonthlyExpenditureE.c
+++ Practical2/MonthlyExpenditureE.c
@@ -1,30 +1,63 @@
 /*Print monthly expenditure 
-Practical 2, Part 2 (e) 
+Practical 2, Part 3 (3) 
 @Nishchal Ranjitkar
 */
 
 #include<stdio.h>
 
 int main(){
-    float foodExpenses;     
+    float foodExpenses;
     float leisureExpenses;
     float clothesExpenses;
     float travelExpenses;
-    float totalSpent;                   
+    float totalSpent;
+    const int accomodation = 500; 
 
-    const int accomodation= 500;
+    while(1){
+        printf("Enter food expenses: ");
+        if(scanf("%f", &foodExpenses)== 1 && foodExpenses>= 0){
+            break;
+        }else{
+            printf("Invalid input. Please enter a positive number.\n");
+            while(getchar()!='\n'); 
+        }
+    }
 
-    printf("Enter food expenses: ");
-    scanf("%f", &foodExpenses);
-    printf("Enter leisure expenses: ");
-    scanf("%f", &leisureExpenses);
-    printf("Enter clothes expenses: ");
-    scanf("%f", &clothesExpenses);
-    printf("Enter travel expenses: ");
-    scanf("%f", &travelExpenses);
-    
-    totalSpent= foodExpenses+ leisureExpenses+ clothesExpenses+ travelExpenses+ accomodation; 
-    printf("The total expenditure this month was £%.2f\n\n", totalSpent);  
+    while(1){
+        printf("Enter leisure expenses: ");
+        if(scanf("%f", &leisureExpenses)== 1 && leisureExpenses>= 0){
+            break;
+        }else{
+            printf("Invalid input. Please enter a positive number.\n");
+            while(getchar()!= '\n'); 
+        }
+    }
+
+    while(1){
+        printf("Enter clothes expenses: ");
+        if(scanf("%f", &clothesExpenses)== 1 && clothesExpenses>= 0){
+            break;
+        }else{
+            printf("Invalid input. Please enter a positive number.\n");
+            while(getchar()!= '\n'); 
+        }
+    }
+
+    while(1){
+        printf("Enter travel expenses: ");
+        if(scanf("%f", &travelExpenses)== 1 && travelExpenses>= 0){
+            break;
+        }else{
+            printf("Invalid input. Please enter a positive number.\n");
+            while(getchar()!= '\n'); 
+        }
+    }
+
+    totalSpent = foodExpenses + leisureExpenses + clothesExpenses + accomodation + travelExpenses;
+
+    printf("\n--------------------------\n");
+    printf("The total expenditure this month was £%.2f\n", totalSpent);
+    printf("----------------------------\n");
 
     return 0;
 }
